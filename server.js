7844c5657cf83f5543e2d91e2428a2fc7e816813
Anthony Ng <newyork.anthonyng@gmail.com>
2016-06-19 22:04:55 -0400

    Add boiler plate for index.html and server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
+app.all('*', (req, res) => {
+	res.redirect('/');
+});
+
+const server = app.listen(process.env.PORT || 3000, () => {
+	console.log('Server running on ' + server.address().port);
+});
