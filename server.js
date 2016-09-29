2c7060b6528f57e6908c4f00e2ffef18342fc71d
Anthony Ng <newyork.anthonyng@gmail.com>
2016-09-29 10:06:31 -0400

    Add gzip compression

--- server.js
+++ server.js
@@ -2,7 +2,9 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const compression = require('compression');
 
+app.use(compression());
 app.use(express.static(path.join(__dirname, 'public'), { maxAge: '30d' }));
 
 app.get('/', (req, res) => {
