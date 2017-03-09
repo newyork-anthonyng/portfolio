a8277ee73befe1b0ec105320077bdd189e95b955
Anthony Ng <newyork.anthonyng@gmail.com>
2017-03-08 21:19:09 -0800

    Update webpack config with file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,10 @@ module.exports = {
         test: /\.css$/,
         loader: 'style-loader!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]'
       },
+      {
+        test: /\.gif$/,
+        loader: 'file-loader?name=[name].script.[ext]'
+      },
     ],
   },
   plugins: [HtmlWebpackPluginConfig],
