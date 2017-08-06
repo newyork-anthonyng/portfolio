658ca4c4c4f32e4d77477d66a2e24bff1763c3e9
Anthony Ng <newyork.anthonyng@gmail.com>
2017-08-05 23:11:54 -0700

    Add service worker

---
+++ sw.js
@@ -0,0 +1,61 @@
+const CACHE_NAME = 'my-site-cache-v1';
+const urlsToCache = [
+  '/',
+  '/styles.css',
+  'https://fonts.googleapis.com/css?family=Nunito+Sans',
+  'https://fonts.gstatic.com/s/nunitosans/v2/iJ4p9wO0GDKJ-D5teKuZqogp9Q8gbYrhqGlRav_IXfk.woff2',
+];
+
+self.addEventListener('install', function(event) {
+  const promise = caches.open(CACHE_NAME)
+    .then(function(cache) {
+      return cache.addAll(urlsToCache);
+    });
+
+  event.waitUntil(promise);
+});
+
+self.addEventListener('fetch', function(event) {
+  const promise = caches.match(event.request)
+    .then(function(response) {
+      // Cache hit - return response
+      if (response) {
+        return response;
+      }
+
+      const fetchRequest = event.request.clone();
+
+      return fetch(fetchRequest).then(function(response) {
+        // Return response without caching if response is not valid
+        if (!response || response !== 200) {
+          return response;
+        }
+
+        const responseToCache = response.clone();
+
+        caches.open(CACHE_NAME)
+          .then(function(cache) {
+            cache.put(event.request, responseToCache);
+          });
+
+        return response;
+      })
+    });
+
+  event.respondWith(promise);
+});
+
+self.addEventListener('activate', function(event) {
+  // Clear out old caches
+  const promise = caches.keys().then(function(cacheNames) {
+    return Promise.all(
+      cacheNames.map(function(cacheName) {
+        if (cacheName !== CACHE_NAME) {
+          return caches.delete(cacheName);
+        }
+      })
+    );
+  });
+
+  event.waitUntil(promise);
+});
