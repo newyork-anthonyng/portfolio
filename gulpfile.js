9506c0ea23f6753acc74fd51c392c74eb4b02b97
Anthony Ng <newyork.anthonyng@gmail.com>
2017-08-08 20:49:11 -0700

    Separate stylesheets

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,18 @@
 var gulp = require('gulp');
+var merge = require('merge-stream');
 var cleanCSS = require('gulp-clean-css');
 var concat = require('gulp-concat');
 var htmlmin = require('gulp-htmlmin');
 
 gulp.task('minify-css', function() {
-  return gulp.src('src/styles.css')
+  var small = gulp.src('src/small.css')
     .pipe(cleanCSS())
     .pipe(gulp.dest('./'));
+  var large = gulp.src('src/large.css')
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('./'));
+
+  return merge(small, large);
 });
 
 gulp.task('minify-html', function() {
