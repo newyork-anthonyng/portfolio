d064464511538327f91a3089f021fa19c3f7d81a
Anthony Ng <newyork.anthonyng@gmail.com>
2020-11-04 17:53:59 -0500

    Simplify page

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,15 @@
-const gulp = require('gulp');
-const cleanCSS = require('gulp-clean-css');
-const htmlmin = require('gulp-htmlmin');
-const uglify = require('gulp-uglify-es').default;
+const gulp = require("gulp");
+const htmlmin = require("gulp-htmlmin");
 
-gulp.task('minify-css', function() {
-  return gulp.src('src/styles.css')
-    .pipe(cleanCSS())
-    .pipe(gulp.dest('./public'));
-});
-
-gulp.task('minify-html', function() {
-  return gulp.src('src/index.html')
-    .pipe(htmlmin({
-      collapseWhiteSpace: true,
-      removeComments: true,
-      minifyJS: true
-    }))
-    .pipe(gulp.dest('./public'));
-});
-
-gulp.task('minify-js', function() {
-  return gulp.src('sw.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('./public'));
-});
-
-gulp.task('copy-assets', ['manifest', 'static']);
-
-gulp.task('manifest', function() {
-  return gulp.src(['manifest.json', 'robots.txt'])
-    .pipe(gulp.dest('./public/'));
-});
-
-gulp.task('static', function() {
-  return gulp.src('static/*')
-    .pipe(gulp.dest('./public/static'));
+gulp.task("minify-html", function () {
+  return gulp
+    .src("src/index.html")
+    .pipe(
+      htmlmin({
+        collapseWhiteSpace: true,
+        removeComments: true,
+        minifyJS: true,
+      })
+    )
+    .pipe(gulp.dest("./public"));
 });
